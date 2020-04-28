129f73561dbf13506b11a95e1b0c1b0937597b9c
Benjamim Sonntag <benjamimsonntag@gmail.com>
2020-04-28 09:22:43 +0100

    Update lint-staged configuration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,4 +2,5 @@ module.exports = {
   '*.css': ['prettier --write'],
   '*.js': ['eslint --fix', 'prettier --write'],
   '*.json': ['prettier --write'],
+  'src/*.js': ['jest --find-related-tests'],
 };
